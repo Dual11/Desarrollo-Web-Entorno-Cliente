5cc9bdc7ca8b9f777ae9566dcde157180b86c32c
alex01 <alextobar013@gmail.com>
2025-10-20 19:40:04 +0200

    prueba

---
+++ practica_1/doc.js
@@ -0,0 +1,12 @@
+ function holaw(){
+           // window.alert("hola windows");
+            //document.write("cosas document");
+            //document.getElementById("ejemInner").innerHTML = "Buenos Dias";
+            console.log(
+                'index.html');
+
+            //alert(2+3);
+            write(5/7);
+
+        }
+        holaw();

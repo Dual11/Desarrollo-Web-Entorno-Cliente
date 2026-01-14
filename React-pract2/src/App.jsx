cd1f1a6c65777838ed6a206810e5421f3f68424e
alex01 <alextobar013@gmail.com>
2026-01-14 18:01:48 +0100

    Practica React 2
    practica react 2 - Alex1

---
+++ React-pract2/src/App.jsx
@@ -0,0 +1,30 @@
+
+// Composicion de componentes
+
+function App() {
+    return <ParentComponent />
+}
+
+function ParentComponent() {
+    return (
+        <div>
+            <UserComponent />
+            <ProfileComponent />
+            <FeedComponent />
+        </div>
+    )
+}
+
+function UserComponent() {
+    return <h2>User component</h2>
+}
+
+function ProfileComponent() {
+    return <h2>Profile component</h2>
+}
+
+function FeedComponent() {
+    return <h2>Feed component</h2>
+}
+
+export default App
